7ac1ed46781c41ef727b580d09fa98df6db8ca03
BenoitCorsini <benoitcorsini@gmail.com>
2023-05-30 16:10:49 +0200

    Info on plates added

--- pixel.py
+++ pixel.py
@@ -26,6 +26,20 @@ class Pixel(PixelInit):
         os.makedirs(self.save_dir)
         self.run()
 
+    def set_image(self, im, figsize=None, extent=None, cmap=None):
+        if figsize is None:
+            figsize = self.pixelplate[::-1]
+        if extent is None:
+            extent = (0, figsize[0], 0, figsize[1])
+        fig = plt.figure(figsize=figsize, dpi=self.dpi)
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        ax = fig.add_subplot()
+        ax.axis('off')
+        ax.set_xlim(xmin=0, xmax=figsize[0])
+        ax.set_ylim(ymin=0, ymax=figsize[1])
+        ax.imshow(im, extent=extent, cmap=cmap)
+        return fig, ax
+
     def get_colour_options(self):
         colour_options = [(
             np.zeros((self.xps, self.yps, 3), dtype=float),
@@ -50,8 +64,8 @@ class Pixel(PixelInit):
         return colour, option
 
     def pixel_im(self):
-        print(f'0% of pixel image: {self.timer()}')
         colour, option = self.get_colour_options()
+        print(f'0% of pixel image: {self.timer()}')
         self.pim = np.zeros((self.nx, self.ny, 3), dtype=float)
         self.nim = np.zeros((self.nx, self.ny), dtype=int)
         xsteps = int(self.nx/self.xps)
@@ -79,31 +93,16 @@ class Pixel(PixelInit):
                 sys.stdout.write('\033[F\033[K')
                 print(f'{perc}% of pixel image: {self.timer()}')
         sys.stdout.write('\033[F\033[K')
-        print(f'Time to pixelize image: {self.timer()}')
-
-    def savepim(self):
-        fig = plt.figure(figsize=(self.ny, self.nx), dpi=self.dpi)
-        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-        ax = fig.add_subplot()
-        ax.axis('off')
-        ax.set_xlim(xmin=0, xmax=self.ny)
-        ax.set_ylim(ymin=0, ymax=self.nx)
-        ax.imshow(self.pim, extent=(0, self.ny, 0, self.nx))
+        fig, ax = self.set_image(self.pim)
         fig.savefig(osp.join(self.save_dir, 'image.png'))
         plt.close()
+        print(f'Time to pixelize image: {self.timer()}')
 
-    def savenim(self):
+    def number_im(self):
         if not osp.exists(osp.join(self.save_dir, 'nims')):
             os.makedirs(osp.join(self.save_dir, 'nims'))
         self.plates = {}
         for num in np.unique(self.nim):
-            fig = plt.figure(figsize=(self.ny, self.nx), dpi=self.dpi)
-            fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-            ax = fig.add_subplot()
-            ax.axis('off')
-            ax.set_xlim(xmin=0, xmax=self.ny)
-            ax.set_ylim(ymin=0, ymax=self.nx)
-            ax.imshow(self.nim != num, cmap='gray', extent=(0, self.ny, 0, self.nx))
             count = int(np.sum(self.nim == num))
             plates = int(np.ceil(count/(6*6*4 - 4)))
             self.plates[int(num)] = {
@@ -111,21 +110,48 @@ class Pixel(PixelInit):
                 'pixels' : count,
                 'extras' : (6*6*4 - 4)*plates - count,
             }
+            fig, ax = self.set_image(self.nim != num, cmap='gray')
             fig.savefig(osp.join(self.save_dir, 'nims', f'{num}:{count}({plates}).png'))
             plt.close()
         with open(osp.join(self.save_dir, 'plates.json'), 'w') as p:
             json.dump(self.plates, p, indent=2)
 
-    def saveplates(self):
+    @staticmethod
+    def draw_grid(ax, figsize, plateshift, platewidth, plateheight, colour='gray', lw=5):
+        for x in range(1 + figsize[0]):
+            ax.plot(
+                [plateshift[0] + x*platewidth/figsize[0]]*2,
+                [plateshift[1], plateshift[1] + plateheight],
+                colour,
+                lw=lw,
+            )
+        for y in range(1 + figsize[1]):
+            ax.plot(
+                [plateshift[0], plateshift[0] + platewidth],
+                [plateshift[1] + y*plateheight/figsize[1]]*2,
+                colour,
+                lw=lw,
+            )
+
+    @staticmethod
+    def draw_info(ax, info, x=2, y=2, fontsize=127, colour='gray'):
+        ax.text(
+            x=x,
+            y=y,
+            s=info,
+            fontsize=fontsize,
+            color=colour,
+        )
+
+    def plates_im(self, platewidth=63*40/100, plateheight=63*50/100):
         print(f'0% of pixel plates: {self.timer()}')
-        if not osp.exists(osp.join(self.save_dir, 'templates')):
-            os.makedirs(osp.join(self.save_dir, 'templates'))
+        if not osp.exists(osp.join(self.save_dir, 'plates')):
+            os.makedirs(osp.join(self.save_dir, 'plates'))
         figsize = (40, 50)
-        xmax = 40
-        ymax = 50
-        plateshift = (4, 5)
-        platewidth = 63*40/100
-        plateheight = 63*50/100
+        plateshift = (
+            (figsize[0] - platewidth)/2,
+            (figsize[1] - plateheight)/2,
+        )
         extent = (
             plateshift[0],
             plateshift[0] + platewidth,
@@ -139,31 +165,14 @@ class Pixel(PixelInit):
                         j*self.pixelplate[1]:(j+1)*self.pixelplate[1],
                 ]
                 for num in np.unique(nim_plate):
-                    fig = plt.figure(figsize=figsize, dpi=self.dpi)
-                    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-                    ax = fig.add_subplot()
-                    ax.axis('off')
-                    ax.set_xlim(xmin=0, xmax=xmax)
-                    ax.set_ylim(ymin=0, ymax=ymax)
                     im = nim_plate != num
-                    if self.pixelplate[0] > self.pixelplate[1]:
+                    if self.pixelplate[0] < self.pixelplate[1]:
                         im = im.T[::-1,:]
-                    ax.imshow(im, cmap='gray', extent=extent)
-                    for z in range(41):
-                        ax.plot(
-                            [plateshift[0] + z*platewidth/40, plateshift[0] + z*platewidth/40],
-                            [plateshift[1], plateshift[1] + plateheight],
-                            'gray',
-                            lw=5
-                        )
-                    for z in range(51):
-                        ax.plot(
-                            [plateshift[0], plateshift[0] + platewidth],
-                            [plateshift[1] + z*plateheight/50, plateshift[1] + z*plateheight/50],
-                            'gray',
-                            lw=5
-                        )
-                    fig.savefig(osp.join(self.save_dir, 'templates', f'{j+1}x{self.xdim-i}({num}).png'))
+                    fig, ax = self.set_image(im, figsize=figsize, cmap='gray', extent=extent)
+                    plate_info = f'plate {j+1}x{self.xdim-i} (colour {num})'
+                    self.draw_grid(ax, figsize, plateshift, platewidth, plateheight)
+                    self.draw_info(ax, plate_info)
+                    fig.savefig(osp.join(self.save_dir, 'plates', plate_info + '.png'))
                     plt.close()
                 perc = int(100*(1 + i*self.ydim + j)/(self.xdim*self.ydim))
                 sys.stdout.write('\033[F\033[K')
@@ -173,7 +182,6 @@ class Pixel(PixelInit):
 
     def run(self):
         self.pixel_im()
-        self.savepim()
         if not self.draft:
-            self.savenim()
-            self.saveplates()
+            self.number_im()
+            self.plates_im()
